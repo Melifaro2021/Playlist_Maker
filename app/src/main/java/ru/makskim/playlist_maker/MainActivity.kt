e8488ccb1d0059576b80a5b777045f4c6f9df30b
MaksBujanov <55047071+MaksBujanov@users.noreply.github.com>
2023-10-06 21:51:22 +0700

    Добавил два Toast 1. Реализация анонимного класса 2. Реализация через Лямбда-выражение

--- app/src/main/java/ru/makskim/playlist_maker/MainActivity.kt
+++ app/src/main/java/ru/makskim/playlist_maker/MainActivity.kt
@@ -15,20 +15,50 @@ class MainActivity : AppCompatActivity() {
         // Кнопка Поиск
         val searchButton = findViewById<Button>(R.id.search)
         searchButton.setOnClickListener {
-            val displayIntent = Intent(this, SearchActivity::class.java)
+            val displayIntent = Intent(
+                this,
+                SearchActivity::class.java)
             startActivity(displayIntent)
         }
         // Кнопка Медиатека
         val mediaButton = findViewById<Button>(R.id.media)
         mediaButton.setOnClickListener {
-            val displayIntent2 = Intent(this, MediatekaActivity::class.java)
+            val displayIntent2 = Intent(
+                this,
+                MediatekaActivity::class.java)
             startActivity(displayIntent2)
         }
-        // Кнопка Настройки
+        /* Кнопка Настройки 1*/
         val settingsButton = findViewById<Button>(R.id.settings)
         settingsButton.setOnClickListener {
-            val displayIntent3 = Intent(this, SettingsActivity::class.java)
+            val displayIntent3 = Intent(
+                this,
+                SettingsActivity::class.java)
             startActivity(displayIntent3)
         }
+
+        /* Кнопка Настройки 2 */
+        /* TOAST Способ 1. Реализация анонимного класса Start
+        val settingsButton = findViewById<Button>(R.id.settings)
+        val imageClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Нажали кнопку Настройки",
+                        Toast.LENGTH_SHORT).show()
+            }
+        }
+        settingsButton.setOnClickListener(imageClickListener)*/
+        //TOAST Способ 1. Реализация анонимного класса END
+
+        /* Кнопка Настройки 3
+        // TOAST Способ 2. Лямбда-выражение Start
+        settingsButton.setOnClickListener {
+            Toast.makeText(
+                this@MainActivity,
+                "Настройки!",
+                Toast.LENGTH_SHORT).show()
+        }*/
+        // TOAST Способ 2. Лямбда-выражение END
     }
 }
