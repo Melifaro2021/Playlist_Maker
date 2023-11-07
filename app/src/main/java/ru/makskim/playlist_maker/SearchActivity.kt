6c5db7f4a3a81d6de5558fc832189b0592cf069a
MaksBujanov <55047071+MaksBujanov@users.noreply.github.com>
2023-11-07 16:46:08 +0700

    Исправляю замечания к Спринту 9 версия 2. Переписал верстку строки поиска при помощи constraintlayout. Сделал скрытие клавиатуры и  др.

--- app/src/main/java/ru/makskim/playlist_maker/SearchActivity.kt
+++ app/src/main/java/ru/makskim/playlist_maker/SearchActivity.kt
@@ -1,90 +1,66 @@
 package ru.makskim.playlist_maker
 
 import android.annotation.SuppressLint
-import android.content.Context
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
-import android.view.View
-import android.view.inputmethod.InputMethodManager
-
-import android.widget.EditText
-import android.widget.FrameLayout
 import android.widget.ImageView
+import android.view.View
+import android.widget.EditText
+import androidx.constraintlayout.widget.ConstraintLayout
+import android.content.Context
+import android.view.inputmethod.InputMethodManager
+import android.text.TextWatcher
+import android.text.Editable
 
 class SearchActivity : AppCompatActivity() {
-    private lateinit var search_query: String
-    private lateinit var searchField: EditText
+    private lateinit var inputEditText: EditText // 1. Поле поиска
+    private lateinit var searchQuery: String // 2. Поле ввода текста
+    private lateinit var searchField: ConstraintLayout // 3. Иконка крестика
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
-        val linearLayout = findViewById<FrameLayout>(R.id.search_field)
-        val inputEditText = findViewById<EditText>(R.id.input_edit_text)
-        val xIco = findViewById<ImageView>(R.id.clear_search_field_icon)
+        searchField = findViewById(R.id.searchField) // 1. Поле поиска
+        inputEditText = findViewById(R.id.inputEditText) // 2. Поле ввода текста
+        val clearButton = findViewById<ImageView>(R.id.clearSearchFieldIcon) // 3. Иконка крестика
 
-        xIco.setOnClickListener {
-            inputEditText.setText("")
+        initBackButton() // Стрелка назад, вызов
+
+        clearButton.visibility = View.INVISIBLE
+        clearButton.setOnClickListener{
+            inputEditText.text.clear() // Очистить поле поиска
+            hideKeyboard() // Скрываем клавиатуру после очистки поля
         }
-
-        /* Кнопка крестика , чтобы стереть поисковый запрос */
-        initClearButton()
-
         // логика по работе с введённым значением
         val simpleTextWatcher = object : TextWatcher {
-            /** beforeTextChanged означает, что символы будут заменены новым теĸстом.
-             * Теĸст не может быть изменён. Это событие используется, ĸогда нам
-             * нужно взглянуть на старый теĸст, ĸоторый вот-вот изменится.**/
-
-            /** Этот метод вызывается, чтобы уведомить вас о том,
-             * что в пределах <code>s</code>:
-             * символы <code>count</code>,
-             * начинающиеся с <code>start</code>
-             * будут заменены новым текстом длиной <code>after</code>. **/
             override fun beforeTextChanged( s: CharSequence?, start: Int, count: Int, after: Int) {
                 // empty
             }
-            /** onTextChanged: внесены изменения, неĸоторые символы тольĸо что были заменены.
-             * Теĸст не может быть изменён. Это событие используется, ĸогда нам нужно увидеть,
-             * ĸаĸие символы в теĸсте являются новыми.**/
             override fun onTextChanged( s: CharSequence?, start: Int, before: Int, count: Int)  {
-              xIco.visibility = clearButtonVisibility(s)
-                search_query = s.toString() // Обновляем searchQuery при изменении текста
+                clearButton.visibility = clearButtonVisibility(s)
+                searchQuery = s.toString() // Обновляем searchQuery при изменении текста
             }
-            /** afterTextChanged: внесены изменения, но теĸст доступен для редаĸтирования.
-             * Это событие используется, ĸогда нам нужно увидеть и, возможно,
-             * отредаĸтировать новый теĸст. **/
             override fun afterTextChanged(s: Editable?) {
                 // empty
             }
         }
         inputEditText.addTextChangedListener(simpleTextWatcher)
-
-        /* Стрелка назад , вызов */
-        initBackButton()
     }
-    /* Стрелка назад, вернуться на предыдущую страницу */
+    // Функция: Стрелка назад, вернуться на предыдущую страницу
     private fun initBackButton() {
         val backButton = findViewById<ImageView>(R.id.arr_back_search_to_main)
         backButton.setOnClickListener {
             finish()
         }
     }
-    // ф-ЦИЯ УДАЛЕНИЯ КЛАВИАТУРЫ НА КРЕСТИК
-    private fun initClearButton(): ImageView? {
-        val clearButton = findViewById<ImageView>(R.id.clear_search_field_icon)
-        clearButton.setOnClickListener {
-            searchField.setText("")
-            /*InputMethodManager — вспомогательный класс, который выступает в роли посредника между
-            источником ввода и приложением. Код ниже может скрыть клавиатуру:*/
-            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
-            inputMethodManager?.hideSoftInputFromWindow(searchField.windowToken, 0)
-        }
-        return clearButton
+    // Функция для скрытия клавиатуры
+    private fun hideKeyboard() {
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+        inputMethodManager?.hideSoftInputFromWindow(inputEditText.windowToken, 0)
     }
-    /* Скрыть или показать крестик в поле поиска */
+    // Функция скрыть или показать крестик в поле поиска
     private fun clearButtonVisibility(s: CharSequence?): Int {
         return if (s.isNullOrEmpty()) {
             View.GONE
@@ -93,25 +69,24 @@ class SearchActivity : AppCompatActivity() {
         }
     }
     /* Хранение данных START */
-    // search_query — это переменная, которую нам нужно сохранить.
-    // KEY — специальный ключ, по которому мы будем сохранять и восстанавливать наше значение.
-    // DEF_SEARCH — это значение по умолчанию для переменной search_query
-
-    private var restored_query:String = DEF_SEARCH
-
     // Переопределяем метод onSaveInstanceState (Сохранение состояния экземпляра),
     // чтобы сохранить текст из EditText (search_query) в Bundle методом putString
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        outState.putString(KEY, search_query)
+        outState.putString(KEY, searchQuery)
     }
+    private var restoredQuery:String = DEF_SEARCH
     // Переопределить метод onRestoreInstanceState(Восстановление состояния экземпляра),
     // чтобы достать данные из Bundle при помощи метода getString и установить их в EditText.
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
-        restored_query = savedInstanceState.getString(KEY, DEF_SEARCH)
+        restoredQuery = savedInstanceState.getString(KEY, DEF_SEARCH)
+        if (restoredQuery != null) {
+            searchQuery = restoredQuery
+            val inputEditText = findViewById<EditText>(R.id.inputEditText)
+            inputEditText.setText(searchQuery)
+        }
     }
-
     // В Kotlin для создания константной переменной мы используем companion object.
     // Ключ должен быть константным, чтобы мы точно знали, что он не изменится
     companion object {
@@ -120,3 +95,4 @@ class SearchActivity : AppCompatActivity() {
     }
     /* Хранение данных END */
 }
+
