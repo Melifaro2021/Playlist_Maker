51add673b9d00732b10a3187ab3162f2da83b336
MaksBujanov <55047071+MaksBujanov@users.noreply.github.com>
2023-11-16 12:49:30 +0700

    10 Спринт.добавил: 1. Ночную тему 2. Вынес размеры, строки

--- app/src/main/java/ru/makskim/playlist_maker/MockPlaylist.kt
+++ app/src/main/java/ru/makskim/playlist_maker/MockPlaylist.kt
@@ -33,5 +33,4 @@ object MockPlaylist {
             "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
         )
     )
-
 }
